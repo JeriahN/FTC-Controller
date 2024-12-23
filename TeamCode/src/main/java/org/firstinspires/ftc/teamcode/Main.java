b9e3bf169e7220e9ad9721f49db02d0bc66bcff4
Jeriah Neal <26jneal@redmondschools.org>
2024-12-23 09:31:24 -0800

    Some Temp Changes, hold on a minute

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Main.java
@@ -1,109 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
-<<<<<<< Updated upstream
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-=======
->>>>>>> Stashed changes
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-<<<<<<< Updated upstream
-import com.acmerobotics.dashboard.FtcDashboard;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.util.Collections;
-import java.util.List;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.firstinspires.ftc.vision.VisionPortal;
-import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
-import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
-import com.qualcomm.robotcore.hardware.CRServo;
-=======
->>>>>>> Stashed changes
 
-// OpenCV Imports
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvPipeline;
-import org.openftc.easyopencv.OpenCvWebcam;
-import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
-import org.opencv.core.Mat;
-
-@TeleOp(name="RevHub Controller with Camera", group="TeleOp")
+@TeleOp(name = "RevHub Controller", group = "TeleOp")
 public class Main extends OpMode {
-<<<<<<< Updated upstream
-    // Motor declarations
-    private DcMotorEx Frm, Blm, Brm, Flm;
-    private DcMotorEx RobotLiftLeft, RobotLiftRight;
-//    private DcMotorEx leftEncoder, rightEncoder, backEncoder;
-    private DcMotorEx ArmLift;
-    private Servo ArmLiftBucketDumper;
-
-//    Servo Declarations
-    private Servo armActuator;
-
-// The intake uses a servo programmed to be continuous to spin an intake wheel
-    private CRServo armActuatorIntake;
-    private Servo intakeDumper;
-
-//    Target Powers
-    private double tgtLeftJoy = 0;
-    private double tgtRightJoy = 0;
-    private double tgtArmLift = 0;
-    private double tgtArmActuator = 0;
-
-    // Localizer
-//    private CustomThreeWheelLocalizer localizer;
-
-    private FtcDashboard dashboard;
-
-    // OpenCV Camera
-    private OpenCvWebcam webcam;
-
-    // Controller input for tank drive
-    double leftStickX;
-    double rightStickX;
-    double leftStickY;
-    double rightStickY;
-    double leftTrigger;
-    double rightTrigger;
-    boolean leftBumper;
-    boolean rightBumper;
-    boolean dPadUp;
-    boolean dPadDown;
-    boolean dPadLeft;
-    boolean dPadRight;
-    boolean a;
-    boolean b;
-    boolean x;
-    boolean y;
-
-    // Controller Variables
-    double leftStickDeadZone = 0.05;
-    double rightStickDeadZone = 0.05;
-    double rightTriggerDeadZone = 0.15;
-
-//    Arm Actuator Variables, the arm actuator is a servo that controls a servo lift based on the angle of the servo, the closer the servo variable gets to its maximum the more extended the actuator gets, the closer it gets to the minimum, the more the scissor lift contracts, d-pad up makes the intake spin in and d-pad down makes the intake spin out
-    private final double actMin = 0;
-    private final double actMax = 55;
-    private final double ALDumperMin = (double) 23 / 100;
-    private final double ALDumperMax = (double) 100 / 100;
-    private double actRange = actMax - actMin;
-
-    private boolean actuatorBucketActive = false;
-    private boolean dPadRightActiveLast = false;
-
-    @Override
-    public void init() {
-//        Connect to hardware classes
-        // Motor initialization
-=======
     //    Hardware Declarations
     private DcMotorEx Frm, Blm, Brm, Flm;
     private Servo clawRotater, claw;
@@ -115,97 +19,17 @@ public class Main extends OpMode {
     boolean clawRotaterActive = false, clawActive = false;
     double frmPower, blmPower, brmPower, flmPower;
 
-    final double clawRotatePosMin = 0, clawRotatePosMax = 210;
-    final double clawPosMin = 0, clawPosMax = 180;
+    final double clawRotatePosMin = 0, clawRotatePosMax = 0.4;
+    final double clawPosMin = 0, clawPosMax = 0.4;
+
 
     public void init() {
         // Initialize motors and servos
->>>>>>> Stashed changes
         Frm = hardwareMap.get(DcMotorEx.class, "Frm");
         Blm = hardwareMap.get(DcMotorEx.class, "Blm");
         Brm = hardwareMap.get(DcMotorEx.class, "Brm");
         Flm = hardwareMap.get(DcMotorEx.class, "Flm");
 
-<<<<<<< Updated upstream
-        RobotLiftLeft = hardwareMap.get(DcMotorEx.class, "RobotLiftLeft");
-        RobotLiftRight = hardwareMap.get(DcMotorEx.class, "RobotLiftRight");
-
-        ArmLift = hardwareMap.get(DcMotorEx.class, "ArmLift");
-        ArmLiftBucketDumper = hardwareMap.get(Servo.class, "ALBDumper");
-
-        // Servo initialization
-        armActuator = hardwareMap.get(Servo.class, "ArmActuator");
-        armActuatorIntake = hardwareMap.get(CRServo.class, "ArmActuatorIntake");
-        intakeDumper = hardwareMap.get(Servo.class, "intakeDumper");
-
-        // Encoder initialization NOT BEING USED RIGHT NOW
-//        leftEncoder = hardwareMap.get(DcMotorEx.class, "leftEncoder");
-//        rightEncoder = hardwareMap.get(DcMotorEx.class, "rightEncoder");
-//        backEncoder = hardwareMap.get(DcMotorEx.class, "backEncoder");
-
-        // Motor directions and modes, the motors need to run without encoders and directions need to be set
-        Frm.setDirection(DcMotorEx.Direction.REVERSE);
-        Blm.setDirection(DcMotorEx.Direction.FORWARD);
-        Brm.setDirection(DcMotorEx.Direction.REVERSE);
-        Flm.setDirection(DcMotorEx.Direction.REVERSE);
-
-        Frm.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-        Blm.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-        Brm.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-        Flm.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-
-        RobotLiftLeft.setDirection(DcMotorEx.Direction.FORWARD);
-        RobotLiftRight.setDirection(DcMotorEx.Direction.FORWARD);
-        RobotLiftLeft.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-        RobotLiftRight.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-
-        ArmLift.setDirection(DcMotorEx.Direction.FORWARD);
-        ArmLift.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-        ArmLiftBucketDumper.setDirection(Servo.Direction.FORWARD);
-        ArmLiftBucketDumper.setPosition(0);
-
-        armActuatorIntake.setDirection(DcMotorEx.Direction.FORWARD);
-
-        armActuator.setDirection(Servo.Direction.FORWARD);
-        armActuator.setPosition(0);
-
-//        localizer = new CustomThreeWheelLocalizer(hardwareMap);
-
-        dashboard = FtcDashboard.getInstance();
-        dashboard.setTelemetryTransmissionInterval(25);
-
-        // Initialize the camera
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
-                "cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
-        WebcamName webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
-        webcam = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
-
-        // Set the pipeline
-        webcam.setPipeline(new SamplePipeline());
-
-        // Open the camera device asynchronously
-        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
-            @Override
-            public void onOpened() {
-                // Start streaming images from the camera
-                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
-
-                // Stream the camera feed to the dashboard
-                FtcDashboard.getInstance().startCameraStream(webcam, 30);
-            }
-
-            @Override
-            public void onError(int errorCode) {
-                telemetry.addData("Error", "Camera could not be opened");
-            }
-        });
-
-        String ipAddress = getRobotControllerIpAddress();
-        String dashboardUrl = "http://" + ipAddress + ":8080/dash"; // FTC Dashboard runs on port 8080
-
-        // Display the URL and dashboard status on telemetry
-        telemetry.addData("FTC Dashboard URL", dashboardUrl);
-=======
         clawRotater = hardwareMap.get(Servo.class, "clawRotater");
         claw = hardwareMap.get(Servo.class, "claw");
 
@@ -219,7 +43,6 @@ public class Main extends OpMode {
         joysticklefty = gamepad1.left_stick_y;
         joystickrightx = gamepad1.right_stick_x;
         joystickrighty = gamepad1.right_stick_y;
->>>>>>> Stashed changes
 
         dPadUp = gamepad1.dpad_up;
         dPadDown = gamepad1.dpad_down;
@@ -237,46 +60,12 @@ public class Main extends OpMode {
         flmPower = joysticklefty;
     }
 
-<<<<<<< Updated upstream
-//    Robot IP Address used to host dashboard and return address
-    public String getRobotControllerIpAddress() {
-        try {
-            List<NetworkInterface> interfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
-            for (NetworkInterface networkInterface : interfaces) {
-                if (networkInterface.isUp() && !networkInterface.isLoopback()) {
-                    List<InetAddress> addresses = Collections.list(networkInterface.getInetAddresses());
-                    for (InetAddress address : addresses) {
-                        if (!address.isLoopbackAddress() && !address.isLinkLocalAddress()) {
-                            String ip = address.getHostAddress();
-                            if (ip != null && !ip.contains(":")) { // Exclude IPv6 addresses
-                                return ip;
-                            }
-                        }
-                    }
-                }
-            }
-        } catch (Exception e) {
-            telemetry.addData("Error", "Could not get IP address");
-        }
-        return "Unknown";
-    }
-
-    @Override
-    public void loop() {
-        // Controller input processing
-        leftStickX = gamepad1.left_stick_x * -1;
-        rightStickX = gamepad1.right_stick_x * -1;
-
-        leftStickY = gamepad1.left_stick_y * -1;
-        rightStickY = gamepad1.right_stick_y * -1;
-=======
     public void loop() {
         // Initialize joysticks
         joystickleftX = gamepad1.left_stick_x;
         joysticklefty = gamepad1.left_stick_y;
         joystickrightx = gamepad1.right_stick_x;
         joystickrighty = gamepad1.right_stick_y;
->>>>>>> Stashed changes
 
         dPadUp = gamepad1.dpad_up;
         dPadDown = gamepad1.dpad_down;
@@ -299,181 +88,48 @@ public class Main extends OpMode {
         brmPower = joysticklefty;
         flmPower = joysticklefty;
 
-<<<<<<< Updated upstream
-
-        // Initialize Power Variables (Reset)
-        double frmSpeed = 0;
-        double blmSpeed = 0;
-        double brmSpeed = 0;
-        double flmSpeed = 0;
-        double targetIntakeSpeed = 0;
-
-//        Robot Lift Controls (Used to hangup robot)
-        if (a) {
-            tgtArmLift = 1;
-        } else if (b) {
-            tgtArmLift = -1;
-        } else {
-            tgtArmLift = 0;
-        }
-
-//        Arm Lift Controls (Used to raise piece to bucket level)
-        if (x) {
-            RobotLiftLeft.setPower(1);
-            RobotLiftRight.setPower(1);
-        } else if (y) {
-            RobotLiftLeft.setPower(-1);
-            RobotLiftRight.setPower(-1);
-        } else {
-            RobotLiftLeft.setPower(0);
-            RobotLiftRight.setPower(0);
-        }
-
-//        Robot Drive Controls
-        // Strafe Controls
-        if (leftBumper) {
-            frmSpeed = -1;
-            blmSpeed = 1;
-        } else if (rightBumper) {
-            frmSpeed = 1;
-            blmSpeed = -1;
-        } 
-
-//        Tank Drive Controls
-        if (Math.abs(leftStickY) > leftStickDeadZone) {
-            flmSpeed = leftStickY;
-            blmSpeed = leftStickY;
-            telemetry.addData("Left Stick Y", leftStickY);
-        }
-
-        if (Math.abs(rightStickY) > rightStickDeadZone) {
-            frmSpeed = rightStickY;
-            brmSpeed = rightStickY;
-            telemetry.addData("Right Stick Y", rightStickY);
-        }
-
-//        Function to toggle DPad right on press
-        if (dPadRight && !dPadRightActiveLast) {
-            actuatorBucketActive = !actuatorBucketActive;
-        }
-        dPadRightActiveLast = dPadRight;
-
-        if (actuatorBucketActive) {
-            intakeDumper.setPosition(ALDumperMax);
-        } else {
-            intakeDumper.setPosition(ALDumperMin);
-        }
-
-        if (dPadUp) {
-            ArmLiftBucketDumper.setPosition(1);
-        } else {
-            ArmLiftBucketDumper.setPosition(0);
-        }
-
-//        Intake Controls (Pickup Piece)
-//        First set the target position and then calculate the correct position to linearize the arm position
-        if (rightTrigger >= rightTriggerDeadZone) {
-            tgtArmActuator = rightTrigger;
-        } else {
-            tgtArmActuator = 0;
-        }
-        armActuator.setPosition(calculateArmActuatorServoPos(tgtArmActuator));
-        if (leftTrigger > 0) {
-            if (dPadDown) {
-                targetIntakeSpeed = leftTrigger;
-            } else {
-                targetIntakeSpeed = -leftTrigger;
-            }
-        }
-
-//        Move piece from intake to bucket which can then be raised and dumped into scoring bin
-
-//        Set Power Levels
-        Frm.setPower(frmSpeed);
-        Blm.setPower(blmSpeed);
-        Brm.setPower(brmSpeed);
-        Flm.setPower(flmSpeed);
-        ArmLift.setPower(tgtArmLift);
-        armActuatorIntake.setPower(targetIntakeSpeed);
-
-//        Odometry Positional Data NOT BEING USED RIGHT NOW
-        // Update the localizer
-//        localizer.update();
-
-        // Get the current estimated pose
-//        Pose2d poseEstimate = localizer.getPoseEstimate();
-
-        // Setup packet to send telemetry data to the dashboard
-        TelemetryPacket packet = new TelemetryPacket();
-
-        // Add pose data to telemetry
-//        telemetry.addData("Pose X", poseEstimate.getX());
-//        telemetry.addData("Pose Y", poseEstimate.getY());
-//        telemetry.addData("Pose Heading", Math.toDegrees(poseEstimate.getHeading()));
-
-        // Add pose data to dashboard packet
-//        packet.put("Pose X", poseEstimate.getX());
-//        packet.put("Pose Y", poseEstimate.getY());
-//        packet.put("Pose Heading", Math.toDegrees(poseEstimate.getHeading()));
-
-        // Send telemetry
-        dashboard.sendTelemetryPacket(packet);
-=======
         // Set motor powers
         Frm.setPower(frmPower);
         Blm.setPower(blmPower);
         Brm.setPower(brmPower);
         Flm.setPower(flmPower);
 
-//        // Control claw rotater with button B
-//        if (bButton) {
-//            clawRotater.setPosition(clawRotatePosMax);
-//            telemetry.addData("Claw Rotater Position", clawRotater.getPosition());
-//        } else if (xButton) {
-//            clawRotater.setPosition(0);
-//            telemetry.addData("Claw Rotater Position", clawRotater.getPosition());
-//        }
-//
-//        else {
-//            //clawRotater.setPosition(clawRotater.getPosition());
-//            telemetry.addData("Claw Rotater Position", clawRotater.getPosition());
-//        }
-//
-//        // Control claw with button A
-//        if (aButton) {
-//            claw.setPosition(claw.getPosition() + 0.1);
-//            telemetry.addData("Claw Position", claw.getPosition());
-//        } else if (yButton) {
-//            claw.setPosition(claw.getPosition() - 0.1);
-//            telemetry.addData("Claw Position", claw.getPosition());
-//        } else {
-//            claw.setPosition(claw.getPosition());
-//            telemetry.addData("Claw Position", claw.getPosition());
-//        }
+        // Control claw rotater with button B
+        if (bButton) {
+            clawRotater.setPosition(clawRotatePosMax);
+            telemetry.addData("Claw Rotater Position", clawRotater.getPosition());
+        } else if (xButton) {
+            clawRotater.setPosition(0);
+            telemetry.addData("Claw Rotater Position", clawRotater.getPosition());
+        }
 
-//        Toggle A, and B buttons on press
-//        if (bButton != bButtonLast) {
-//            if (bButton) {
-//                clawRotaterActive = !clawRotaterActive;
-//                telemetry.addData("Claw Rotater Active", clawRotaterActive);
-//            }
-//            bButtonLast = bButton;
-//            telemetry.addData("Claw Rotater Active", clawRotaterActive);
-//        }
+        else {
+            //clawRotater.setPosition(clawRotater.getPosition());
+            telemetry.addData("Claw Rotater Position", clawRotater.getPosition());
+        }
 
-//        if (clawRotaterActive) {
+        // Control claw with button A
+        if (aButton) {
+            claw.setPosition(90);
+            telemetry.addData("Claw Position", claw.getPosition());
+        } else if (yButton) {
+            claw.setPosition(0);
+            telemetry.addData("Claw Position", claw.getPosition());
+        } else {
+            claw.setPosition(claw.getPosition());
+            telemetry.addData("Claw Position", claw.getPosition());
+        }
+
+        if (bButton != bButtonLast) {
             if (bButton) {
-                if (bButtonLast) {
-                    clawRotater.setPosition((clawRotatePosMin));
-                    bButtonLast = false;
-                    telemetry.addData("Claw Rotater Position", clawRotater.getPosition());
-                } else {
-                    clawRotater.setPosition(clawRotatePosMax);
-                    bButtonLast = true;
-                    telemetry.addData("Claw Rotater Position", clawRotater.getPosition());
-                }
+                clawRotaterActive = !clawRotaterActive;
+                telemetry.addData("Claw Rotater Active", clawRotaterActive);
             }
-//        }
+            bButtonLast = bButton;
+            telemetry.addData("Claw Rotater Active", clawRotaterActive);
+        }
+
+        
 
 
 
@@ -506,45 +162,16 @@ public class Main extends OpMode {
         }
 
         telemetry.addData("Status", "Running");
->>>>>>> Stashed changes
+
         telemetry.update();
     }
 
     public void stop() {
-        // Stop all motors when the op mode ends
         Frm.setPower(0);
         Blm.setPower(0);
         Brm.setPower(0);
         Flm.setPower(0);
-<<<<<<< Updated upstream
-        armActuator.setPosition(0);
-        telemetry.addData("Status", "Stopped");
-        telemetry.update();
-
-        // Stop the camera
-        if (webcam != null) {
-            webcam.stopStreaming();
-            FtcDashboard.getInstance().stopCameraStream();
-        }
-    }
-
-    public double calculateArmActuatorServoPos(double targetPosition) {
-        return actMin + (targetPosition / actRange) * (actMax - actMin);
-    }
-
-    // Sample OpenCV Pipeline
-    public static class SamplePipeline extends OpenCvPipeline {
-        @Override
-        public Mat processFrame(Mat input) {
-            // Process the input frame (e.g., image processing tasks)
-
-            // For now, just return the input frame
-            return input;
-        }
-=======
         clawRotater.setPosition(0);
         claw.setPosition(0);
->>>>>>> Stashed changes
     }
 }
-
